a5642368a250c4fcf902db32aa61788eadd3cf60
OjukwuEbuka <ebukaojukwu23@gmail.com>
2020-10-20 16:05:58 +0100

    First Commit - setup server, db, graphql resolvers and typedefs

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require('apollo-server');
+
+const { sequelize } = require('./models');
+
+// A map of functions which return data for the schema.
+const resolvers = require('./graphql/resolvers');
+
+// Types Declarations
+const typeDefs = require('./graphql/typeDefs');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+
+    sequelize.authenticate()
+        .then(() => console.log('Database connected!'))
+        .catch((err) => console.log(err));
+})
