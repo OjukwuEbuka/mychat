368af5a2e3199d9f566f7394321e3bd30276c6c6
OjukwuEbuka <ebukaojukwu23@gmail.com>
2020-10-22 21:09:06 +0100

    Wrote the login graphql query, used jsonwebtoken jwt for user auuthentication

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -3,11 +3,15 @@ const { gql } = require('apollo-server');
 module.exports = gql`
     type Query{
         getUsers: [User]!
+        login(username:String!, password: String!): User!
+        me: User!
     }
 
     type User {
         username: String!
         email: String!
+        token: String
+        createdAt: String!
     }
     type Mutation {
         register(
