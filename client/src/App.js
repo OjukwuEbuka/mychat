f55ec48d8235ecc85bf05f0dff4abde451bc16f7
OjukwuEbuka <ebukaojukwu23@gmail.com>
2020-10-23 16:09:10 +0100

    Setup Register page complete fxnality with error handling and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.scss';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
 import Register from './pages/Register';
+import Login from './pages/Login';
+
+import ApolloProvider from './ApolloProvider'
 
 function App() {
 
   return(
-    <Container className="pt-5">
-      <Register />
-    </Container>
+    <ApolloProvider>
+      <BrowserRouter>
+        <Switch>
+          <Container className="pt-5">
+              <Route exact path="/" component={Home} />
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+          </Container>
+        </Switch>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,47 @@
 import React, { useState } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
+import { gql, useMutation  } from '@apollo/client';
+
+const REGISTER_USER = gql`
+    mutation Register(
+        $email: String!, 
+        $username: String!, 
+        $password: String!, 
+        $confirmPassword: String!
+    ){
+        register(
+            email: $email, 
+            username: $username, 
+            password: $password, 
+            confirmPassword: $confirmPassword
+        ){
+            username
+            email
+            createdAt
+        }
+    }
+`
 
 const Register = () => {
     
-  const [variables, setVariables] = useState({
-    email: "",
-    username: "",
-    password: "",
-    cpassword: ""
-  })
+    const [variables, setVariables] = useState({
+      email: "",
+      username: "",
+      password: "",
+      confirmPassword: ""
+    });
+
+    const [errors, setErrors] = useState({});
+    
+    const [registerUser, { loading }] = useMutation(REGISTER_USER, {
+        update(_, res){
+            console.log(res);
+        },
+        onError(err){
+            console.log(err.graphQLErrors[0].extensions.errors);
+            setErrors(err.graphQLErrors[0].extensions.errors);
+        }
+    });
 
   const inputVal = e => {
     setVariables({...variables, [e.target.name]: e.target.value})
@@ -17,7 +50,7 @@ const Register = () => {
   const submitRegisterForm = e => {
     e.preventDefault();
     // setVariables
-    console.log(variables);
+    registerUser({ variables });
   }
   return (
     <Row className="bg-white py-5 justify-content-center">
@@ -25,23 +58,51 @@ const Register = () => {
             <h1 className="text-center">Register</h1>
             <Form onSubmit={submitRegisterForm}>
             <Form.Group>
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" name="email" onChange={inputVal} value={variables.email} />
+                <Form.Label className={errors.email && 'text-danger'}>
+                    { errors.email ??'Email Address'}
+                </Form.Label>
+                <Form.Control 
+                    type="email" 
+                    name="email" 
+                    className={ errors.email && 'is-invalid'}
+                    onChange={inputVal} value={variables.email} 
+                />
             </Form.Group>
             <Form.Group>
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" name="username" onChange={inputVal} value={variables.username} />
+                <Form.Label className={errors.username && 'text-danger'}>
+                    {errors.username ?? 'Username'}
+                </Form.Label>
+                <Form.Control 
+                    type="text" 
+                    name="username" 
+                    className={ errors.username && 'is-invalid'}
+                    onChange={inputVal} value={variables.username} 
+                />
             </Form.Group>
             <Form.Group>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" name="password" onChange={inputVal} value={variables.password} />
+                <Form.Label className={errors.password && 'text-danger'}>
+                    { errors.password ?? 'Password'}
+                </Form.Label>
+                <Form.Control 
+                    type="password" 
+                    name="password" 
+                    className={ errors.password && 'is-invalid'}
+                    onChange={inputVal} value={variables.password} 
+                />
             </Form.Group>
             <Form.Group>
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" name="cpassword" onChange={inputVal} value={variables.cpassword} />
+                <Form.Label className={errors.confirmPassword && 'text-danger'}>
+                    { errors.confirmPassword ?? 'Confirm Password'}
+                </Form.Label>
+                <Form.Control 
+                    type="password" 
+                    name="confirmPassword" 
+                    className={ errors.confirmPassword && 'is-invalid'}
+                    onChange={inputVal} value={variables.confirmPassword} 
+                />
             </Form.Group>
-            <Button variant="success" type="submit">
-                Register
+            <Button variant="success" type="submit" disabled={loading}>
+                { loading ? 'Loading' : 'Register'}
             </Button>
             </Form>
         </Col>
